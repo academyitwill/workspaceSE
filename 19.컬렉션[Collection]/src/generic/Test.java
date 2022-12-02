235a056b3fc23846d058d8d9df298a328476f8fc
academyitwill <academyitwill@gmail.com>
2022-12-02 18:15:33 +0900

    .

--- 19.컬렉션[Collection]/src/generic/Test.java
+++ 19.컬렉션[Collection]/src/generic/Test.java
@@ -3,8 +3,11 @@ package generic;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("sdsads");
+		System.out.println("sdsads");
+		System.out.println("sdsads");
+		System.out.println("sdsads");
+		System.out.println("sdsads");
 	}
 
 }
